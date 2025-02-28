70d2b61b358cf951b055c97552833df3d3b06f45
IliyanSinapov <iliyan.sinapov@gmail.com>
2025-03-01 00:04:23 +0200

    added some stuff

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,16 @@
 return {
   "nvimtools/none-ls.nvim",
   config = function()
-      local null_ls = require("null-ls")
-
+    local null_ls = require("null-ls")
 
     null_ls.setup({
-        sources = {
-            null_ls.builtins.formatting.stylua,
-                  null_ls.builtins.completion.spell
-      }
-        })
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
-      vim.keymap.set('n', '<S-M-F>', vim.lsp.buf.format, {})
-  end
+    vim.keymap.set("n", "<S-M-F>", vim.lsp.buf.format, {})
+  end,
 }
