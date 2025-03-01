8f22ac53c1e76d89f4490d77b9a813d499b159df
IliyanSinapov <iliyan.sinapov@gmail.com>
2025-03-01 15:20:24 +0200

    added some stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,35 +1,46 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
+        lazy = false,
+        opts = {
+          auto_install = true,
+        },
         ensure_installed = {
           "lua_ls",
           "jdtls",
           "eslint",
-          "ts_ls"
-        }
+          "ts_ls",
+        },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
       lspconfig.jdtls.setup({})
       lspconfig.eslint.setup({})
       lspconfig.ts_ls.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,4 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.g.mapleader = " "
 vim.opt.number = true
+vim.opt.relativenumber = true
